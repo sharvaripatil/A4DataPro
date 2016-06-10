56768b20e69eb4f827d554aae2a88ad49abeade1
Amey <amey.more@a4technology.com>
2016-06-10 14:38:48 +0530

    2nd part v2 completed

--- A4techExcelConvertionTool/src/main/java/com/a4tech/v2/criteria/parser/ProductImprintColorParser.java
+++ A4techExcelConvertionTool/src/main/java/com/a4tech/v2/criteria/parser/ProductImprintColorParser.java
@@ -5,35 +5,40 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+
+
+
+import com.a4tech.util.ApplicationConstants;
+import com.a4tech.v2.core.model.ImprintColor;
 import com.a4tech.v2.core.model.ImprintColorValue;
 
 
 public class ProductImprintColorParser { 
 	
 	private Logger              _LOGGER              = Logger.getLogger(getClass());
-	public List<ImprintColorValue> getImprintColorCriteria(String imprintColorValue){
-		List<ImprintColorValue> impcolorValuesList =new ArrayList<ImprintColorValue>();
+	public ImprintColor getImprintColorCriteria(String imprintColorValue){
+		ImprintColor imprintColorObj= new ImprintColor();
 		try{
 		String impValue = imprintColorValue;
-		String imprintArr[] = impValue.split("\\|");
-		
-		 
+		String imprintArr[] = impValue.split(ApplicationConstants.CONST_DELIMITER_SPLITTING_PIPE);
+		List<ImprintColorValue> impcolorValuesList =new ArrayList<ImprintColorValue>();
+		imprintColorObj.setType("COLR");
 		ImprintColorValue impclrObj=null;
 		
 		
 		for (String tempImpint : imprintArr) {
  			impclrObj=new ImprintColorValue();
- 			impclrObj.setName(tempImpint.trim());
+ 			impclrObj.setName(tempImpint);
  			impcolorValuesList.add(impclrObj);
 		}
- 		 
+ 		imprintColorObj.setValues(impcolorValuesList);
 		}catch(Exception e){
 			_LOGGER.error("Error while processing Imprint Color :"+e.getMessage());           
-			
+			return null;
    	
    }
  		
- 		return impcolorValuesList;
+ 		return imprintColorObj;
 		
 		
 	}
