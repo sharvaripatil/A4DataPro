c14864c8cb791466de3ea6a672b3cf0a3c125ea4
Sharvari <sharvari.patil@a4technology.com>
2016-09-01 12:04:52 +0530

    commit with all parser

---
+++ A4techExcelConvertionTool/main/java/supplier/91561ESPTemplate/com/a4tech/product/ESPTemplate/parser/RushTimeParser.java
@@ -0,0 +1,32 @@
+package com.a4tech.product.ESPTemplate.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.a4tech.product.model.RushTime;
+import com.a4tech.product.model.RushTimeValue;
+import com.a4tech.util.ApplicationConstants;
+
+public class RushTimeParser {
+	
+	
+	public RushTime getRushTimeValues(String rushTime ,String rushService){
+		List<RushTimeValue> RushTimevalueList   = new ArrayList<RushTimeValue>();
+		RushTime RushTimeObj=new RushTime();
+		
+		if(rushService.equalsIgnoreCase(ApplicationConstants.CONST_CHAR_Y))
+		{
+		RushTimeObj.setAvailable(true);
+		RushTimeValue RushTimeValueObj=new RushTimeValue();
+		RushTimeValueObj.setBusinessDays(rushTime);	
+		RushTimeValueObj.setDetails(ApplicationConstants.CONST_STRING_EMPTY);
+		RushTimevalueList.add(RushTimeValueObj);
+		
+		RushTimeObj.setRushTimeValues(RushTimevalueList);
+		}
+		
+		
+		return RushTimeObj;	
+	}
+
+}
