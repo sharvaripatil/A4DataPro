166e5842e6f0961cc6244d6e14a7d11c2374438e
Amey <amey.more@a4technology.com>
2016-11-22 16:12:08 +0530

    Completed Options & Availability -42057.

--- A4techExcelConvertionTool/src/main/java/com/a4tech/util/ApplicationConstants.java
+++ A4techExcelConvertionTool/src/main/java/com/a4tech/util/ApplicationConstants.java
@@ -144,7 +144,8 @@ public class ApplicationConstants {
 	
 	 public static Map<String, String> COLOR_MAP =new HashMap<String, String>();
 	 public static Map<String, String> SIZE_MAP =new HashMap<String, String>();
-	
+	 public static Map<String, String> OPTION_MAP =new HashMap<String, String>();
+	 
 	  public static Map<String, String> SUPPLIER_EMAIL_ID_MAP =new HashMap<String, String>();
 	  public static Map<String, String> SUPPORT_EMAIL_ID_MAP =new HashMap<String, String>();
 	    static {
@@ -1359,6 +1360,10 @@ public class ApplicationConstants {
 	    	SIZE_MAP.put("XTLx10","Standard & Numbered___10");
 	    	SIZE_MAP.put("XTLx12","Standard & Numbered___12");
 	    	SIZE_MAP.put("XXLxEMPTY","Standard & Numbered___2XL");
+	    	OPTION_MAP.put("REG","Regular");
+	    	OPTION_MAP.put("SHT","Short");
+	    	OPTION_MAP.put("TLL","Tall");
+	    	OPTION_MAP.put("XTL","Extra Tall");
 	    }
     
 }
