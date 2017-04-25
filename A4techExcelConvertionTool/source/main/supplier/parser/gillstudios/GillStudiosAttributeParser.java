d5eaf0788e8cc9d6cac8ba7f00f4fabe67c93ad1
Amey <amey.more@a4technology.com>
2017-04-25 12:01:09 +0530

    Partial Commit -GillStudios

---
+++ A4techExcelConvertionTool/source/main/supplier/parser/gillstudios/GillStudiosAttributeParser.java
@@ -0,0 +1,374 @@
+package parser.gillstudios;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import parser.tomaxusa.TomaxConstants;
+import com.a4tech.product.model.Color;
+import com.a4tech.product.model.Combo;
+import com.a4tech.product.model.Dimensions;
+import com.a4tech.product.model.Image;
+import com.a4tech.product.model.ImprintSize;
+import com.a4tech.product.model.Material;
+import com.a4tech.product.model.NumberOfItems;
+import com.a4tech.product.model.Origin;
+import com.a4tech.product.model.Packaging;
+import com.a4tech.product.model.Product;
+import com.a4tech.product.model.ProductConfigurations;
+import com.a4tech.product.model.RushTime;
+import com.a4tech.product.model.RushTimeValue;
+import com.a4tech.product.model.Shape;
+import com.a4tech.product.model.ShippingEstimate;
+import com.a4tech.product.model.Value;
+import com.a4tech.product.model.Values;
+import com.a4tech.product.model.Weight;
+import com.a4tech.supplier.mapper.GillStudiosMapping;
+import com.a4tech.util.ApplicationConstants;
+import com.a4tech.util.CommonUtility;
+
+public class GillStudiosAttributeParser {
+	private static final Logger _LOGGER = Logger.getLogger(GillStudiosAttributeParser.class);
+	
+public Product getExistingProductData(Product existingProduct , ProductConfigurations existingProductConfig){
+		
+		ProductConfigurations newProductConfigurations=new ProductConfigurations();
+		Product newProduct=new Product();
+		
+		try{
+			//categories
+			List<String> listCategories=new ArrayList<String>();
+			listCategories=existingProduct.getCategories();
+			if(!CollectionUtils.isEmpty(listCategories)){
+				newProduct.setCategories(listCategories);
+			}
+		//Images
+		List<Image> images=existingProduct.getImages();
+		if(!CollectionUtils.isEmpty(images)){
+			newProduct.setImages(images);
+		}
+		//keywords
+		List<String> productKeywords=existingProduct.getProductKeywords();
+		if(!CollectionUtils.isEmpty(productKeywords)){
+			newProduct.setProductKeywords(productKeywords);
+		}
+		//product colors
+		List<Color> colors =existingProductConfig.getColors();
+		if(!CollectionUtils.isEmpty(colors)){
+			newProductConfigurations.setColors(colors);
+		}
+		//shape
+		List<Shape> shapes =existingProductConfig.getShapes();
+		if(!CollectionUtils.isEmpty(productKeywords)){
+			newProductConfigurations.setShapes(shapes);
+		}
+		//material
+		List<Material>   materials=existingProductConfig.getMaterials();
+		if(!CollectionUtils.isEmpty(productKeywords)){
+			newProductConfigurations.setMaterials(materials);
+		}
+		newProduct.setProductConfigurations(newProductConfigurations);
+		}catch(Exception e){
+			_LOGGER.error("Error while processing Existing Product Data " +e.getMessage());
+		}
+		 _LOGGER.info("Completed processing Existing Data");
+		return newProduct;
+		
+	}
+	
+	
+	public List<Values> getValues(String dimensionValue, String dimensionUnits,
+			String dimensionType) {
+
+		String dimensionValueArr[] = dimensionValue
+				.split(ApplicationConstants.CONST_DIMENSION_SPLITTER);
+		String dimensionUnitsArr[] = dimensionUnits
+				.split(ApplicationConstants.CONST_DIMENSION_SPLITTER);
+		String dimensionTypeArr[] = dimensionType
+				.split(ApplicationConstants.CONST_DIMENSION_SPLITTER);
+
+		ArrayList<Value> valueList = new ArrayList<Value>();
+		List<Values> valueslist = new ArrayList<Values>();
+
+		Values valuesObj = new Values();
+		Value valueObj = null;
+
+		for (int i = 0; i < dimensionValueArr.length; i++) {
+			valueObj = new Value();
+			valueObj.setValue(dimensionValueArr[i]);
+			valueObj.setUnit(GillStudiosLookupData.Dimension1Units.get(dimensionUnitsArr[i]));
+			valueObj.setAttribute(GillStudiosLookupData.Dimension1Type.get(dimensionTypeArr[i]));
+			valueList.add(valueObj);
+		}
+
+		valuesObj.setValue(valueList);
+		valueslist.add(valuesObj);
+
+		return valueslist;
+	}
+	
+	public List<Origin> getOriginValues(String originValue){
+		List<Origin> listOfOrigin   = new ArrayList<Origin>();
+		Origin origin = new Origin();
+		if(originValue.equalsIgnoreCase(ApplicationConstants.CONST_STRING_COUNTRY_CODE_CH)){
+			originValue = ApplicationConstants.CONST_STRING_COUNTRY_NAME_CHINA;
+		}else if(originValue.equalsIgnoreCase(ApplicationConstants.CONST_STRING_COUNTRY_CODE_US)){
+			originValue = ApplicationConstants.CONST_STRING_COUNTRY_NAME_USA;
+		}else if(originValue.equalsIgnoreCase("CN")){
+			originValue = ApplicationConstants.CONST_STRING_COUNTRY_NAME_CHINA;
+		}else{
+			
+		}
+		origin.setName(originValue);
+		listOfOrigin.add(origin);
+		return listOfOrigin;
+	}
+  
+	public String  getCountryCodeConvertName(String countryCode){
+		String countryName = null;
+		if(countryCode.equalsIgnoreCase(ApplicationConstants.CONST_STRING_COUNTRY_CODE_CH)){
+			countryName = ApplicationConstants.CONST_STRING_COUNTRY_NAME_CHINA;
+		}else if(countryCode.equalsIgnoreCase(ApplicationConstants.CONST_STRING_COUNTRY_CODE_US)){
+			countryName = ApplicationConstants.CONST_STRING_COUNTRY_NAME_USA;
+		}else{
+			
+		}
+		return countryName;
+	}
+	
+	public RushTime getRushTimeValues(String rushTime ,RushTime existingRushTime){
+		existingRushTime.setAvailable(ApplicationConstants.CONST_BOOLEAN_TRUE);
+		List<RushTimeValue> existingRushTimeValues = existingRushTime.getRushTimeValues();
+		if(existingRushTimeValues == null){
+			existingRushTimeValues = new ArrayList<RushTimeValue>();
+		}
+		RushTimeValue newRushTimeValue = new RushTimeValue();
+		newRushTimeValue.setBusinessDays(rushTime);
+		newRushTimeValue.setDetails(ApplicationConstants.CONST_STRING_EMPTY);
+		existingRushTimeValues.add(newRushTimeValue);
+		existingRushTime.setRushTimeValues(existingRushTimeValues);
+		return existingRushTime;
+	}
+	
+	public List<Packaging> getPackageValues(String packageValues){
+		List<Packaging> listOfPackage = new ArrayList<Packaging>();
+		Packaging packaging = null;
+		if(packageValues.contains(ApplicationConstants.CONST_DELIMITER_COMMA)){
+			String[] packValues = packageValues.split(ApplicationConstants.CONST_DELIMITER_COMMA);
+			for (String pack : packValues) {
+				packaging = new Packaging();
+			   packaging.setName(pack);
+			   listOfPackage.add(packaging);
+			}
+		}else{
+			packaging = new Packaging();
+			   packaging.setName(packageValues);
+			   listOfPackage.add(packaging);
+		}
+		return listOfPackage;
+		
+	}
+	
+	
+	public ShippingEstimate getShippingEstimateValues(String length,String width,String height,
+				                                         String weight,String numberOfItems){
+			ShippingEstimate shippingEstimate = new ShippingEstimate();
+			Dimensions dimension = null;
+			List<NumberOfItems> numberOfItem = null;
+			List<Weight> listOfWeightObj = null;
+			Weight  weightObj = null;
+			NumberOfItems numberOfItemsObj = null;
+			if(!length.equals(ApplicationConstants.CONST_STRING_ZERO)){
+				dimension = new Dimensions();
+				dimension.setLength(length);
+				dimension.setWidth(width);
+				dimension.setHeight(height);
+				dimension.setLengthUnit(ApplicationConstants.CONST_STRING_INCHES);
+				dimension.setWidthUnit(ApplicationConstants.CONST_STRING_INCHES);
+				dimension.setHeightUnit(ApplicationConstants.CONST_STRING_INCHES);
+			}
+			if(!weight.equals(ApplicationConstants.CONST_STRING_ZERO)){
+				listOfWeightObj = new ArrayList<Weight>();
+				weightObj = new Weight();
+				weightObj.setValue(weight);
+				weightObj.setUnit(ApplicationConstants.CONST_STRING_SHIPPING_WEIGHT);
+				listOfWeightObj.add(weightObj);
+			}
+			if(!numberOfItems.equals(ApplicationConstants.CONST_STRING_ZERO)){
+				numberOfItem = new ArrayList<NumberOfItems>();
+				numberOfItemsObj = new NumberOfItems();
+				numberOfItemsObj.setValue(numberOfItems);
+				numberOfItemsObj.setUnit(ApplicationConstants.CONST_STRING_SHIPPING_NUMBER_UNIT_CARTON);
+				numberOfItem.add(numberOfItemsObj);
+			}
+			if(dimension != null){
+				shippingEstimate.setDimensions(dimension);
+			}
+			if(numberOfItem != null){
+				shippingEstimate.setNumberOfItems(numberOfItem);
+			}
+			if(listOfWeightObj != null){
+				shippingEstimate.setWeight(listOfWeightObj);
+			}
+			
+			return shippingEstimate;
+	}
+	
+	
+	
+	public List<ImprintSize> getimprintsize(StringBuilder firstImprintSize) {
+			
+			String ImprintSizeValue=firstImprintSize.toString().replace("null x null","");
+			ImprintSizeValue=ImprintSizeValue.replace("null", "");
+		    List<ImprintSize> imprintSizeList =new ArrayList<ImprintSize>();
+		    ImprintSize impsizeobj;
+		    
+			String ImprintsizeArr[]=ImprintSizeValue.split(",");
+			
+			
+		   for (String Value : ImprintsizeArr) {
+			   impsizeobj=new ImprintSize();
+			   impsizeobj.setValue(Value);
+			   imprintSizeList.add(impsizeobj);
+		      }		
+			
+			
+			
+			return imprintSizeList;
+	}
+	// color parsing
+	@SuppressWarnings("unused")
+	public List<Color> getProductColors(String color){
+		List<Color> listOfColors = new ArrayList<>();
+		try{
+		Color colorObj = null;
+		color=color.replaceAll("\\|",",");
+		String[] colors =getValuesOfArray(color, ",");
+		for (String colorName : colors) {
+			if(StringUtils.isEmpty(colorName)){
+				continue;
+			}
+			colorName=colorName.replaceAll("&","/");
+			colorName=colorName.replaceAll(" w/","/");
+			colorName=colorName.replaceAll(" W/","/");
+			//colorName = colorName.trim();
+			
+			colorObj = new Color();
+			String colorGroup = TomaxConstants.getColorGroup(colorName.trim());
+			//if (colorGroup == null) {
+				//if (colorGroup!=null && colorGroup.contains(ApplicationConstants.CONST_DELIMITER_FSLASH)) {
+			if (colorName.contains("/") || colorGroup.contains(ApplicationConstants.CONST_DELIMITER_FSLASH)) {
+				
+				if(colorGroup==null){
+					colorGroup=colorName;
+				}
+				colorGroup=colorGroup.replaceAll("&","/");
+				colorGroup=colorGroup.replaceAll(" w/","/");
+				colorGroup=colorGroup.replaceAll(" W/","/");
+				
+				//if (colorName.contains(ApplicationConstants.CONST_DELIMITER_FSLASH)) {
+					if(isComboColor(colorGroup)){
+						List<Combo> listOfCombo = null;
+						String[] comboColors = CommonUtility.getValuesOfArray(colorGroup,
+								ApplicationConstants.CONST_DELIMITER_FSLASH);
+						String colorFirstName = TomaxConstants.getColorGroup(comboColors[0].trim());
+						colorObj.setName(colorFirstName == null?"Other":colorFirstName);
+						int combosSize = comboColors.length;
+						if (combosSize == ApplicationConstants.CONST_INT_VALUE_TWO) {
+							String colorComboFirstName = TomaxConstants.getColorGroup(comboColors[1].trim());
+							colorComboFirstName = colorComboFirstName == null?"Other":colorComboFirstName;
+							listOfCombo = getColorsCombo(colorComboFirstName, ApplicationConstants.CONST_STRING_EMPTY,
+									combosSize);
+						} else{
+							String colorComboFirstName = TomaxConstants.getColorGroup(comboColors[1].trim());
+							colorComboFirstName = colorComboFirstName == null?"Other":colorComboFirstName;
+							
+							String colorComboSecondName = TomaxConstants.getColorGroup(comboColors[2].trim());
+							colorComboSecondName = colorComboSecondName == null?"Other":colorComboSecondName;
+							listOfCombo = getColorsCombo(colorComboFirstName,colorComboSecondName, combosSize);
+						}
+						String alias = colorGroup.replaceAll(ApplicationConstants.CONST_DELIMITER_FSLASH, "-");
+						colorObj.setAlias(alias);
+						colorObj.setCombos(listOfCombo);
+					} else {
+						String[] comboColors = CommonUtility.getValuesOfArray(colorGroup,
+								ApplicationConstants.CONST_DELIMITER_FSLASH);
+						String mainColorGroup = TomaxConstants.getColorGroup(comboColors[0].trim());
+						if(mainColorGroup != null){
+							colorObj.setName(mainColorGroup);
+							colorObj.setAlias(colorName);
+						} else {
+							colorObj.setName(ApplicationConstants.CONST_VALUE_TYPE_OTHER);
+							colorObj.setAlias(colorName);
+						}
+					}
+				/*} else {
+					if (colorGroup == null) {
+					colorGroup = ApplicationConstants.CONST_VALUE_TYPE_OTHER;
+					}
+					colorObj.setName(colorGroup);
+					colorObj.setAlias(colorName);
+				}*/
+			} else {
+				if (colorGroup == null) {
+					colorGroup = ApplicationConstants.CONST_VALUE_TYPE_OTHER;
+					}
+				colorObj.setName(colorGroup);
+				colorObj.setAlias(colorName);
+			}
+			listOfColors.add(colorObj);
+		}
+		}catch(Exception e){
+			_LOGGER.error("Error while processing color: "+e.getMessage());
+		}
+		return listOfColors;
+	}
+	private List<Combo> getColorsCombo(String firstValue,String secondVal,int comboLength){
+		List<Combo> listOfCombo = new ArrayList<>();
+		Combo comboObj1 = new Combo();
+		Combo comboObj2 = new Combo();
+		comboObj1.setName(firstValue);
+		comboObj1.setType(ApplicationConstants.CONST_STRING_SECONDARY);
+		comboObj2.setName(secondVal);
+		comboObj2.setType(ApplicationConstants.CONST_STRING_TRIM);
+		if(comboLength == ApplicationConstants.CONST_INT_VALUE_TWO){
+			listOfCombo.add(comboObj1);
+		} else {
+			listOfCombo.add(comboObj1);
+			listOfCombo.add(comboObj2);
+		}
+		return listOfCombo;
+	}
+	
+	public static boolean isComboColor(String colorValue){
+    	String[] colorVals = CommonUtility.getValuesOfArray(colorValue, "/");
+    	String mainColor       = null;
+    	String secondaryColor  = null;
+    	String thirdColor      = null;
+    	if(colorVals.length == ApplicationConstants.CONST_INT_VALUE_TWO){
+    		 mainColor = TomaxConstants.getColorGroup(colorVals[0].trim());
+    		 secondaryColor = TomaxConstants.getColorGroup(colorVals[1].trim());
+    		 if(mainColor != null && secondaryColor != null){
+    			 return true;
+    		 }
+    	} else if(colorVals.length == ApplicationConstants.CONST_INT_VALUE_THREE){
+    		 mainColor      = TomaxConstants.getColorGroup(colorVals[0].trim());
+    		 secondaryColor = TomaxConstants.getColorGroup(colorVals[1].trim());
+    		 thirdColor     = TomaxConstants.getColorGroup(colorVals[2].trim());
+    		 if(mainColor != null && secondaryColor != null && thirdColor != null){
+    			 return true;
+    		 }
+    	} else{
+    		
+    	}
+    	return false;
+    }
+	
+	public static String[] getValuesOfArray(String data,String delimiter){
+		   if(!StringUtils.isEmpty(data)){
+			   return data.split(delimiter);
+		   }
+		   return null;
+	   }
+}
---
+++ A4techExcelConvertionTool/source/main/supplier/parser/gillstudios/GillStudiosImprintMethodParser.java
@@ -0,0 +1,77 @@
+package parser.gillstudios;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.a4tech.lookup.service.LookupServiceData;
+import com.a4tech.lookup.service.restService.LookupRestService;
+import com.a4tech.product.model.ImprintMethod;
+
+public class GillStudiosImprintMethodParser {
+	
+	private LookupServiceData lookupServiceDataObj;
+	private LookupRestService lookupRestServiceObj;
+	
+	public List<ImprintMethod> getImprintMethodValues(String imprintMethod,
+			                                           List<ImprintMethod> imprintMethodList){
+		List<ImprintMethod> imprintMethodsList = new ArrayList<ImprintMethod>();
+		imprintMethod=imprintMethod.replace("4-color process", "Full Color");
+		imprintMethod=imprintMethod.replace("Pad printed", "Pad Print");
+		imprintMethod=imprintMethod.replace("Screen printed", "Silkscreen");
+		ImprintMethod imprMethod = new ImprintMethod();
+		
+		if(imprintMethod.contains("Laser"))
+		{
+			imprMethod = new ImprintMethod();
+			 imprMethod.setAlias("Laser Engraved");
+			 imprMethod.setType("Laser Engraved");
+			 imprintMethodsList.add(imprMethod);
+		}
+
+		else{
+		
+		List<String> finalImprintValues = getImprintValue(imprintMethod.toUpperCase().trim());	
+		for (String innerValue : finalImprintValues) {
+		    	 imprMethod = new ImprintMethod();
+				 imprMethod.setAlias(innerValue);
+				 imprMethod.setType(innerValue);
+				 imprintMethodsList.add(imprMethod);  
+		}
+		}
+		return imprintMethodsList;
+	}
+
+	
+	public List<String> getImprintValue(String value){
+		List<String> imprintLookUpValue = lookupServiceDataObj.getImprintMethods();
+		List<String> finalImprintValues = imprintLookUpValue.stream()
+				                                  .filter(impntName -> value.contains(impntName))
+				                                  .collect(Collectors.toList());
+                                                 
+				
+		return finalImprintValues;	
+	}
+
+	public LookupServiceData getLookupServiceDataObj() {
+		return lookupServiceDataObj;
+	}
+
+	public void setLookupServiceDataObj(LookupServiceData lookupServiceDataObj) {
+		this.lookupServiceDataObj = lookupServiceDataObj;
+	}
+
+	public LookupRestService getLookupRestServiceObj() {
+		return lookupRestServiceObj;
+	}
+
+	public void setLookupRestServiceObj(LookupRestService lookupRestServiceObj) {
+		this.lookupRestServiceObj = lookupRestServiceObj;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
