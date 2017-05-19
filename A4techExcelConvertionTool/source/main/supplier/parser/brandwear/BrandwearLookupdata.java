071213aa286fe81312a744e0ac72af6f881b995e
Sharvari <sharvari.patil@a4technology.com>
2017-05-19 10:50:55 +0530

    added LOOkUP data brandwear supplier

---
+++ A4techExcelConvertionTool/source/main/supplier/parser/brandwear/BrandwearLookupdata.java
@@ -0,0 +1,88 @@
+package parser.brandwear;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class BrandwearLookupdata {
+
+	public static Map<String, String> COLOR_MAP =new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+
+	static{
+		
+		COLOR_MAP.put("White","Medium White");
+		COLOR_MAP.put("Magenta","Medium Pink");
+		COLOR_MAP.put("Black","Medium Black");
+		COLOR_MAP.put("Red","Medium Red");
+		COLOR_MAP.put("Apple Green","Medium Green");
+		COLOR_MAP.put("Charcoal Heather","Medium Gray");
+		COLOR_MAP.put("Brilliant Blue","Bright Blue");
+		COLOR_MAP.put("Aquarius","Light Blue");
+		COLOR_MAP.put("Navy","Dark Blue");
+		COLOR_MAP.put("Gray Mix","Medium Gray");
+		COLOR_MAP.put("Charcoal Mix","Dark Gray");
+		COLOR_MAP.put("Plum","Medium Purple");
+		COLOR_MAP.put("Orange Peel","Medium Orange");
+		COLOR_MAP.put("Paprika","Medium Red");
+		COLOR_MAP.put("Latte","Light Brown");
+		COLOR_MAP.put("Raspberry","Dark Pink");
+		COLOR_MAP.put("Green","Medium Green");
+		COLOR_MAP.put("Persian Blue","Medium Blue");
+		COLOR_MAP.put("Peacock","Medium Blue");
+		COLOR_MAP.put("Iris","Medium Blue");
+		COLOR_MAP.put("Brown","Medium Brown");
+		COLOR_MAP.put("Salt & Pepper","Medium Black");
+		COLOR_MAP.put("Charcoal","Medium Gray");
+		COLOR_MAP.put("Silver","Medium Gray");
+		COLOR_MAP.put("Fresh Lime","Medium Green");
+		COLOR_MAP.put("Wild Pink","Medium Pink");
+		COLOR_MAP.put("Cool Aqua","Medium Blue");
+		COLOR_MAP.put("Royal","Bright Blue");
+		COLOR_MAP.put("Mosaic Blue","Medium Blue");
+		COLOR_MAP.put("Purple","Medium Purple");
+		COLOR_MAP.put("Hot Pink","Bright Pink");
+		COLOR_MAP.put("Tangerine","Medium Orange");
+		COLOR_MAP.put("Mosaic Gray","Medium Gray");
+		COLOR_MAP.put("Graphite","Medium Gray");
+		COLOR_MAP.put("Moasic Blue","Medium Blue");
+		COLOR_MAP.put("Citrus","Medium Blue");
+		COLOR_MAP.put("Lollipop","Medium Pink");
+		COLOR_MAP.put("Hi-Liter","Bright Yellow");
+		COLOR_MAP.put("Herringbone","Medium Gray");
+		COLOR_MAP.put("Licorice","Medium Black");
+		COLOR_MAP.put("Cherry Burst","Medium Pink");
+		COLOR_MAP.put("Hi-Liter Yellow","Bright Yellow");
+		COLOR_MAP.put("Tan","Light Brown");
+		COLOR_MAP.put("Gray","Medium Gray");
+		COLOR_MAP.put("Blue","Medium Blue");
+		COLOR_MAP.put("Crimson Tide","Dark Red");
+		COLOR_MAP.put("Dark Navy","Dark Blue");
+		COLOR_MAP.put("Sky Blue","Light Blue");
+		COLOR_MAP.put("Pink","Medium Pink");
+		COLOR_MAP.put("Red Source","Medium Red");
+		COLOR_MAP.put("Dark Gold","Dark Yellow");
+		COLOR_MAP.put("Baby Blue","Light Blue");
+		COLOR_MAP.put("Heather Grey","Medium Gray");
+		COLOR_MAP.put("Cobalt","Medium Blue");
+		COLOR_MAP.put("Orange","Medium Orange");
+		COLOR_MAP.put("Forest","Medium Green");
+		COLOR_MAP.put("Chocolate","Medium Brown");
+		COLOR_MAP.put("Rose","Medium Red");
+		COLOR_MAP.put("Pure White","Medium White");
+		COLOR_MAP.put("Maroon","Dark Red");
+		COLOR_MAP.put("Moss","Medium Green");
+		COLOR_MAP.put("Mango","Medium Orange");
+		COLOR_MAP.put("Fire Blue","Medium Blue");
+		COLOR_MAP.put("Beige","Light Brown");
+		COLOR_MAP.put("Mosaic Grey","Medium Gray");
+		COLOR_MAP.put("Grey Mix","Medium Gray");
+		COLOR_MAP.put("Black/Black","Medium Black");
+	
+		
+		
+		
+		
+		
+	}
+	
+	
+}
