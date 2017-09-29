53679185446e88a91a4c5d4b773950c51c6985f6
Venkat <Venkat@172.25.175.35>
2017-09-29 18:42:06 +0530

    final change schedular time for client

--- A4techExcelConvertionTool/src/main/java/com/a4tech/product/service/imple/MailServiceImpl.java
+++ A4techExcelConvertionTool/src/main/java/com/a4tech/product/service/imple/MailServiceImpl.java
@@ -86,16 +86,16 @@ public class MailServiceImpl implements IMailService{
 		      MimeMessage mimeMessage = mailSender.createMimeMessage();
 		      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false);
 		      helper.setFrom(senderMailName);
-		      String[] toAddress = {"venkateswarlu.nidamanuri@a4technology.com","sharvari.patil@a4technology.com",
+		      String[] bccmails = {"venkateswarlu.nidamanuri@a4technology.com","sharvari.patil@a4technology.com",
 		    		  "amey.more@a4technology.com"};
 			/*String[] ccAddress = { "venkateswarlu.nidamanuri@a4technology.com", "sharvari.patil@a4technology.com",
 					"amey.more@a4technology.com","azam.rizvi@a4technology.com" };*/
-			helper.setTo(toAddress); 
-			//helper.setTo("SPullins@asicentral.com");
-			//helper.setBcc(bccmails);
+			//helper.setTo(toAddress); 
+			helper.setTo("SPullins@asicentral.com");
+			helper.setBcc(bccmails);
 		      helper.setSubject(subject);
 		      helper.setText(body);
-			_LOGGER.info("Sending Email to : "+ Arrays.toString(toAddress));
+			_LOGGER.info("Sending Email to : "+ Arrays.toString(bccmails));
 			   mailSender.send(mimeMessage);
 		       _LOGGER.info("Process Status Mail Sent Successfully !!!");
 		      } catch (MessagingException e) {
@@ -115,15 +115,15 @@ public class MailServiceImpl implements IMailService{
 		      helper.setFrom(senderMailName);
 		      /*String[] toAddress = {"venkateswarlu.nidamanuri@a4technology.com","sharvari.patil@a4technology.com",
 		    		  "azam.rizvi@a4technology.com","amey.more@a4technology.com"};*/
-		      String[] toAddress = { "venkateswarlu.nidamanuri@a4technology.com", "sharvari.patil@a4technology.com",
-				"amey.more@a4technology.com","azam.rizvi@a4technology.com" };
-		      helper.setTo(toAddress);
-		      //helper.setTo("SPullins@asicentral.com");
-		      //helper.setBcc(bccmails);
+		      String[] bccmails = { "venkateswarlu.nidamanuri@a4technology.com", "sharvari.patil@a4technology.com",
+				"amey.more@a4technology.com"};
+		     // helper.setTo(toAddress);
+		      helper.setTo("SPullins@asicentral.com");
+		      helper.setBcc(bccmails);
 		      helper.setSubject(subject);
 		      helper.setText(body);
 		      helper.addAttachment(file.getFilename(), file);
-			_LOGGER.info("Sending Email to : "+ Arrays.toString(toAddress));
+			_LOGGER.info("Sending Email to : "+ Arrays.toString(bccmails));
 			   mailSender.send(mimeMessage);
 		       _LOGGER.info("Process Status Mail Sent Successfully !!!");
 		      } catch (MessagingException e) {
