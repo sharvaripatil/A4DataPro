4801cb150889cbd791ab78c605074ad13ca9c885
Venkat <Venkat@192.168.1.48>
2018-11-23 11:28:38 +0530

    Added products delete mapper classes and code has been changes in fileupload class

---
+++ A4techExcelConvertionTool/main/java/supplier/47791BestDeal/com/a4tech/bestDeal/product/mapping/DeleteProductsMapping.java
@@ -0,0 +1,82 @@
+package com.a4tech.bestDeal.product.mapping;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import com.a4tech.bestDeal.product.parser.BestDealAttributeParser;
+import com.a4tech.bestDeal.product.parser.BestDealPriceGridParser;
+import com.a4tech.product.dao.service.ProductDao;
+import com.a4tech.product.service.postImpl.PostServiceImpl;
+import com.a4tech.util.ApplicationConstants;
+import com.a4tech.util.CommonUtility;
+
+public class DeleteProductsMapping {
+	
+	private static final Logger _LOGGER = Logger.getLogger(DeleteProductsMapping.class);
+
+	public List<String> getAllXids(Workbook workbook ,Integer asiNumber ,int batchId, String environmentType){
+		  List<String> xidsList = new ArrayList<>();
+ 		try{
+	    Sheet sheet = workbook.getSheetAt(0);
+		Iterator<Row> iterator = sheet.iterator();
+		String xid = null;
+		
+		while (iterator.hasNext()) {
+			
+			try{
+			Row nextRow = iterator.next();
+			if(nextRow.getRowNum() == ApplicationConstants.CONST_NUMBER_ZERO){
+				continue;
+			}
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+				
+				int columnIndex = cell.getColumnIndex();
+				if(columnIndex  == 0){
+					Cell xidCell = nextRow.getCell(1);
+				     xid = CommonUtility.getCellValueStrinOrInt(xidCell);
+					//xid = CommonUtility.getCellValueStrinOrInt(cell);
+				}
+				switch (columnIndex+1) {
+				case 1://xid
+					  xidsList.add(xid);
+					 break;
+			
+			}  // end inner while loop
+					 
+		}
+				
+			}catch(Exception e){
+		}
+		}
+		workbook.close();
+	       return xidsList;
+		}catch(Exception e){
+			_LOGGER.error("Error while Processing excel sheet " +e.getMessage());
+			return xidsList;
+		}finally{
+			try {
+				workbook.close();
+			} catch (IOException e) {
+				_LOGGER.error("Error while Processing excel sheet" +e.getMessage());
+	
+			}
+				_LOGGER.info("Complted processing of excel sheet ");
+				
+		}
+		
+	}
+
+	
+
+
+}
--- A4techExcelConvertionTool/src/main/java/com/a4tech/controller/FileUpload.java
+++ A4techExcelConvertionTool/src/main/java/com/a4tech/controller/FileUpload.java
@@ -1,14 +1,22 @@
 package com.a4tech.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +30,7 @@ import com.a4tech.product.dao.service.ProductDao;
 import com.a4tech.product.service.ILoginService;
 import com.a4tech.product.service.IMailService;
 import com.a4tech.product.service.IProductService;
+import com.a4tech.product.service.postImpl.PostServiceImpl;
 import com.a4tech.util.ApplicationConstants;
 import com.a4tech.util.CommonUtility;
 import com.a4tech.util.ConvertCsvToExcel;
@@ -42,17 +51,18 @@ public class FileUpload {
 	private ConvertCsvToExcel     convertCsvToExcel;
 	private ExcelFactory          excelFactory;
 	private LookupData            lookupData;
+	private PostServiceImpl postServiceImpl;
 
 	private static Logger _LOGGER = Logger.getLogger(Class.class);
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String welcomePage(Map<String, Object> model) {
-		/*FileBean fileBean = new FileBean();
+		FileBean fileBean = new FileBean();
 		model.put("filebean", fileBean);
-		return ApplicationConstants.CONST_STRING_HOME;*/
-		FtpLoginBean ftpLogin = new FtpLoginBean(); //uncommented code while using ftp
+		return ApplicationConstants.CONST_STRING_HOME;
+		/*FtpLoginBean ftpLogin = new FtpLoginBean(); //uncommented code while using ftp
 		model.put("ftpLoginBean", ftpLogin);
-		return "ftpLogin";
+		return "ftpLogin";*/
 		}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -87,16 +97,23 @@ public class FileUpload {
 			if(workbook == null){
 				return ApplicationConstants.CONST_STRING_ERROR_PAGE;
 			}
-			//lookupData.loadLineNames(asiNumber, accessToken);
-			//lookupData.loadFobPoints(asiNumber, accessToken);
 			int batchId = productDao.createBatchId(Integer.parseInt(asiNumber));
+			
+			// this code used to delete all products 
+			/*List<String> xidsList = getAllXids(workbook);
+			finalResult = deleteProducts(xidsList, accessToken, "Sand", Integer.valueOf(asiNumber), batchId);
+			if (finalResult != null) {
+				parseFinalData(finalResult, asiNumber, batchId, redirectAttributes);
+			}
+	    	return ApplicationConstants.CONST_REDIRECT_URL;*/
+			
 			request.getSession().setAttribute("batchId",
 					String.valueOf(batchId));
 			IExcelParser parserObject = excelFactory.getExcelParserObject(asiNumber);
 			if(parserObject != null){ // new implemention
 				//if file upload for Production ,please change the environemnt Type "Sand" to "Prod"
 				finalResult = parserObject.readExcel(accessToken, workbook, 
-                        Integer.valueOf(asiNumber), batchId,"Sand");//here change environment type
+                        Integer.valueOf(asiNumber), batchId,"Prod");//here change environment type
 		    	if (finalResult != null) {
 					parseFinalData(finalResult, asiNumber, batchId, redirectAttributes);
 				}
@@ -149,6 +166,78 @@ public class FileUpload {
 			
 		}*/
 	}
+	public List<String> getAllXids(Workbook workbook){
+		  List<String> xidsList = new ArrayList<>();
+		try{
+	    Sheet sheet = workbook.getSheetAt(0);
+		Iterator<Row> iterator = sheet.iterator();
+		String xid = null;
+		
+		while (iterator.hasNext()) {
+			
+			try{
+			Row nextRow = iterator.next();
+			if(nextRow.getRowNum() == ApplicationConstants.CONST_NUMBER_ZERO){
+				continue;
+			}
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+				
+				int columnIndex = cell.getColumnIndex();
+				if(columnIndex  == 0){
+					Cell xidCell = nextRow.getCell(0);
+				     xid = CommonUtility.getCellValueStrinOrInt(xidCell);
+					//xid = CommonUtility.getCellValueStrinOrInt(cell);
+				}
+				switch (columnIndex+1) {
+				case 1://xid
+					if(!StringUtils.isEmpty(xid)){
+						xidsList.add(xid);
+					}
+					 break;
+			
+			}  // end inner while loop
+					 
+		}
+				
+			}catch(Exception e){
+		}
+		}
+		workbook.close();
+	       return xidsList;
+		}catch(Exception e){
+			_LOGGER.error("Error while Processing excel sheet " +e.getMessage());
+			return xidsList;
+		}finally{
+			try {
+				workbook.close();
+			} catch (IOException e) {
+				_LOGGER.error("Error while Processing excel sheet" +e.getMessage());
+	
+			}
+				_LOGGER.info("Complted processing of excel sheet ");
+				
+		}
+		
+	}
+	private String deleteProducts(List<String> xidsList,String accessToekn,String environmentType,int asiNumber,int batchId){
+		List<String> numOfProductsSuccess = new ArrayList<String>();
+		for (String xid : xidsList) {
+			int num;
+			try {
+				num = postServiceImpl.deleteProduct(accessToken, xid, asiNumber, batchId,environmentType);
+				if(num ==1){
+					numOfProductsSuccess.add("1");
+				}
+			} catch (IOException e) {
+				_LOGGER.error("Unable to delete xid:"+xid);
+			}
+				
+		}
+		String finalResult = numOfProductsSuccess.size() + ",0";
+		return finalResult;
+	}
 	public IProductService getProductService() {
 		return productService;
 	}
@@ -185,5 +274,13 @@ public class FileUpload {
 	public void setLookupData(LookupData lookupData) {
 		this.lookupData = lookupData;
 	}
+	public PostServiceImpl getPostServiceImpl() {
+		return postServiceImpl;
+	}
+
+	public void setPostServiceImpl(PostServiceImpl postServiceImpl) {
+		this.postServiceImpl = postServiceImpl;
+	}
+
 
 }
